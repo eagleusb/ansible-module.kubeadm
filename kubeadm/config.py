ea9bf45bcb4dfa4981d9205cbf6149791668795a
eagleusb <git@eagleusb.com>
2019-08-24 14:42:43 +0200

    feat(config) add default kubeadm config

--- kubeadm/config.py
+++ kubeadm/config.py
@@ -1,3 +1,38 @@
+KUBEADM_TEMPLATE_VARS = {
+  "init": {
+    "apiVersion": "kubeadm.k8s.io/v1beta2",
+    "apiAddress": "0.0.0.0",
+    "apiPort": "6443",
+    "kVersion": "stable-1",
+    # "clusterName": "foobar",
+    "imageRepo": "k8s.gcr.io",
+    # "controlPlaneEndpoint": "",
+    "tlsVersion": "VersionTLS12",
+    "dns": {
+      "type": "CoreDNS",
+      "domain": "cluster.local",
+    },
+    "network": {
+      "podSubnet": "10.244.0.0/16",
+      "serviceSubnet": "10.96.0.0/12",
+    },
+    "cert": {
+      "sans": [],
+    },
+    "rate": {
+      "maxBytes": "131072",
+      "maxMutatingReqs": "200",
+      "maxReqs": "400",
+    },
+    "etcd": {
+      "endpoints": [],
+      "ca": "/etc/kubernetes/pki/etcd/ca.crt",
+      "cert": "/etc/kubernetes/pki/etcd/client.crt",
+      "key": "/etc/kubernetes/pki/etcd/client.key",
+    }
+  },
+}
+
 KUBEADM_MOD_CONFIG = {
   "support_check_mode": True,
   # "required_together": [['init', 'name']],
@@ -11,13 +46,12 @@ KUBEADM_ARG_SPEC = {
   "init": {
     "type": "bool",
     "default": False,
+    "choices": ['yes', 'no', True, False],
+    "aliases": ['create', 'initialize'],
   },
-  "force": {
-    "type": "bool",
-    "default": False,
-  },
-  "name": {
-    "type": "str",
-    "required": False,
+  "config": {
+    "type": "dict",
+    "default": KUBEADM_TEMPLATE_VARS,
+    "aliases": ['cfg', 'configuration'],
   },
 }
