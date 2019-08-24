7c83f9dcbf3656e945969be750bcaa251648fa34
eagleusb <git@eagleusb.com>
2019-08-24 14:44:52 +0200

    feat(template) process init jinja template and attach itto object

--- kubeadm/template.py
+++ kubeadm/template.py
@@ -1,61 +1,41 @@
 from jinja2 import Environment, FileSystemLoader
 from pathlib import Path
+from config import KUBEADM_TEMPLATE_VARS
+import os
+import tempfile
 
-KUBEADM_TEMPLATE_VARS = {
-  "init": {
-    "apiVersion": "kubeadm.k8s.io/v1beta2",
-    "apiAddress": "0.0.0.0",
-    "apiPort": "6443",
-    "kVersion": "stable-1",
-    # "clusterName": "foobar",
-    "imageRepo": "k8s.gcr.io",
-    # "controlPlaneEndpoint": "",
-    "tlsVersion": "VersionTLS12",
-    "dns": {
-      "type": "CoreDNS",
-      "domain": "cluster.local",
-    },
-    "network": {
-      "podSubnet": "10.244.0.0/16",
-      "serviceSubnet": "10.96.0.0/12",
-    },
-    "cert": {
-      "sans": [],
-    },
-    "rate": {
-      "maxBytes": "131072",
-      "maxMutatingReqs": "200",
-      "maxReqs": "400",
-    },
-    "etcd": {
-      "endpoints": [],
-      "ca": "/etc/kubernetes/pki/etcd/ca.crt",
-      "cert": "/etc/kubernetes/pki/etcd/client.crt",
-      "key": "/etc/kubernetes/pki/etcd/client.key",
-    }
-  },
-}
 
 class KubeadmTemplate(object):
-  def __init__(self, *args, **kwargs):
-    # super().__init__(*args, **kwargs)
+  __slots__ = ['__jinja', '__loaded', '__rendered', 'written']
+
+  def __init__(self, kind):
     current_directory = str(Path(__file__).parent.resolve())
-    self.env = Environment(
+    self.__jinja = Environment(
       loader = FileSystemLoader(current_directory + '/templates'),
       trim_blocks = True,
       lstrip_blocks = True,
     )
+    self.templatize(kind)
 
   def __load(self, template):
-    self.loaded = self.env.get_template(template + '.yml.j2')
+    self.__loaded = self.__jinja.get_template(template + '.yml.j2')
 
   def __render(self, kind):
-    self.rendered = self.loaded.render(KUBEADM_TEMPLATE_VARS[kind])
+    self.__rendered = bytes(self.__loaded.render(KUBEADM_TEMPLATE_VARS[kind]), encoding='utf-8')
+
+  def __write(self, kind):
+    fd, tmpfile = tempfile.mkstemp(prefix=f'kubeadm_{kind}_')
+    with open(tmpfile, 'wb') as file:
+      file.write(self.__rendered)
+    os.close(fd)
+    self.written = tmpfile
 
   def templatize(self, template):
     self.__load(template)
     self.__render(template)
-    print(self.rendered)
+    self.__write(template)
+
 
 if __name__ == '__main__':
-  KubeadmTemplate().templatize('init')
+  test = KubeadmTemplate('init')
+  print(test.__dict__)
