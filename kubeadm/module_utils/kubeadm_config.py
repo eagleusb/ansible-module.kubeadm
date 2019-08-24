b1947df7d127b590d569fac4c1fe4c083ed99bed
eagleusb <git@eagleusb.com>
2019-08-24 23:44:04 +0200

    feat(moduleUtils) use module utils ansible loader for config and template

--- kubeadm/module_utils/kubeadm_config.py
+++ kubeadm/module_utils/kubeadm_config.py
@@ -1,4 +1,5 @@
-{% set etcdEndpoints = etcd.endpoints %}
+KUBEADM_TEMPLATE_SPEC = {
+    "init": """{% set etcdEndpoints = etcd.endpoints %}
 {% set apiCertSans = cert.sans -%}
 
 ---
@@ -18,16 +19,17 @@ imageRepository: "{{ imageRepo | default('k8s.gcr.io') }}"
 {% if controlPlaneEndpoint is string %}
 controlPlaneEndpoint: "{{ controlPlaneEndpoint }}:{{ apiPort }}"
 {% endif %}
+{% if etcdEndpoints is sequence() and (etcdEndpoints | length()) is odd() %}
 etcd:
   external:
     endpoints:
-{% if etcdEndpoints is sequence() and (etcdEndpoints | length()) is odd() %}
-{% for endpoint in etcdEndpoints %}      - "{{ endpoint }}"
-{% endfor %}
+{% for endpoint in etcdEndpoints %}      - "{{ endpoint }}"{% endfor %}
 {% endif %}
+{% if etcd.cert is defined %}
     caFile: "{{ etcd.ca | default() }}"
     certFile: "{{ etcd.cert | default() }}"
     keyFile: "{{ etcd.key | default() }}"
+{% endif %}
 apiServer:
 {% if apiCertSans is sequence() and (apiCertSans | length()) > 0 %}
   certSANs:
@@ -76,4 +78,68 @@ kind: KubeletConfiguration
 
 ---
 apiVersion: kubeproxy.config.k8s.io/v1alpha1
-kind: KubeProxyConfiguration
+kind: KubeProxyConfiguration""",
+    "join": """---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: JoinConfiguration
+caCertPath: /etc/kubernetes/pki/ca.crt
+discovery:
+  bootstrapToken:
+    apiServerEndpoint: kube-apiserver:6443
+    token: abcdef.0123456789abcdef
+    unsafeSkipCAVerification: true
+  timeout: 5m0s
+  tlsBootstrapToken: abcdef.0123456789abcdef
+nodeRegistration:
+  criSocket: /var/run/dockershim.sock
+  name: eagleusb
+  taints: null""",
+}
+
+
+KUBEADM_TEMPLATE_VARS = {
+    "init": {
+        "apiVersion": "kubeadm.k8s.io/v1beta2",
+        "apiAddress": "0.0.0.0",
+        "apiPort": "6443",
+        "kVersion": "stable-1",
+        # "clusterName": "foobar",
+        "imageRepo": "k8s.gcr.io",
+        # "controlPlaneEndpoint": "",
+        "tlsVersion": "VersionTLS12",
+        "dns": {"type": "CoreDNS", "domain": "cluster.local"},
+        "network": {"podSubnet": "10.244.0.0/16", "serviceSubnet": "10.96.0.0/12"},
+        "cert": {"sans": []},
+        "rate": {"maxBytes": "131072", "maxMutatingReqs": "200", "maxReqs": "400"},
+        "etcd": {
+            "endpoints": [],
+            # "cert": {
+            #   "ca": "/etc/kubernetes/pki/etcd/ca.crt",
+            #   "cert": "/etc/kubernetes/pki/etcd/client.crt",
+            #   "key": "/etc/kubernetes/pki/etcd/client.key",
+            # },
+        },
+    }
+}
+
+
+KUBEADM_MOD_CONFIG = {
+    "support_check_mode": True,
+    # "required_together": [['init', 'name']],
+}
+
+
+KUBEADM_ARG_SPEC = {
+    "version": {"type": "bool", "default": False},
+    "init": {
+        "type": "bool",
+        "default": False,
+        "choices": ["yes", "no", True, False],
+        "aliases": ["create", "initialize"],
+    },
+    "config": {
+        "type": "dict",
+        "default": KUBEADM_TEMPLATE_VARS,
+        "aliases": ["cfg", "configuration"],
+    },
+}
---
+++ kubeadm/module_utils/kubeadm_template.py
@@ -0,0 +1,47 @@
+import os
+import tempfile
+
+from jinja2 import Environment, DictLoader
+from pathlib import Path
+
+from ansible.module_utils.kubeadm_config import (
+    KUBEADM_TEMPLATE_SPEC,
+    KUBEADM_TEMPLATE_VARS,
+)
+
+
+class KubeadmTemplate(object):
+    __slots__ = ["__jinja", "__loaded", "__rendered", "written"]
+
+    def __init__(self, kind):
+        self.__jinja = Environment(
+            loader=DictLoader(KUBEADM_TEMPLATE_SPEC),
+            trim_blocks=True,
+            lstrip_blocks=True,
+        )
+        self.templatize(kind)
+
+    def _load(self, template):
+        self.__loaded = self.__jinja.get_template(template)
+
+    def _render(self, kind):
+        self.__rendered = bytes(
+            self.__loaded.render(KUBEADM_TEMPLATE_VARS[kind]), encoding="utf-8"
+        )
+
+    def _write(self, kind):
+        fd, tmpfile = tempfile.mkstemp(prefix=f"kubeadm_{kind}_")
+        with open(tmpfile, "wb") as file:
+            file.write(self.__rendered)
+        os.close(fd)
+        self.written = tmpfile
+
+    def templatize(self, template):
+        self._load(template)
+        self._render(template)
+        self._write(template)
+
+
+if __name__ == "__main__":
+    test = KubeadmTemplate("init")
+    print(test.__dict__)
